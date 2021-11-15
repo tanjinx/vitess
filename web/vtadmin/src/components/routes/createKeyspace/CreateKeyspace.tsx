56c25bf514f20a6cad1c6a4b51ec14aa4c8c0e7a
Sara Bee <855595+doeg@users.noreply.github.com>
2021-11-14 20:47:35 -0500

    xxx
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/components/routes/createKeyspace/CreateKeyspace.tsx
+++ web/vtadmin/src/components/routes/createKeyspace/CreateKeyspace.tsx
@@ -23,36 +23,11 @@ interface FormState {
 
 const DEFAULT_FORM_STATE: FormState = {
     clusterID: 'local',
-    name: 'kitties',
+    name: '',
 };
-// // Name is the name of the keyspace.
-// string name = 1;
-// // Force proceeds with the request even if the keyspace already exists.
-// bool force = 2;
-// // AllowEmptyVSchema allows a keyspace to be created with no vschema.
-// bool allow_empty_v_schema = 3;
 
-// // ShardingColumnName specifies the column to use for sharding operations.
-// string sharding_column_name = 4;
-// // ShardingColumnType specifies the type of the column to use for sharding
-// // operations.
-// topodata.KeyspaceIdType sharding_column_type = 5;
-
-// // ServedFroms specifies a set of db_type:keyspace pairs used to serve
-// // traffic for the keyspace.
-// repeated topodata.Keyspace.ServedFrom served_froms = 6;
-
-// // Type is the type of the keyspace to create.
-// topodata.KeyspaceType type = 7;
-// // BaseKeyspace specifies the base keyspace for SNAPSHOT keyspaces. It is
-// // required to create a SNAPSHOT keyspace.
-// string base_keyspace = 8;
-// // SnapshotTime specifies the snapshot time for this keyspace. It is required
-// // to create a SNAPSHOT keyspace.
-// vttime.Time snapshot_time = 9;
 export const CreateKeyspace = () => {
     const { data: clusters = [], ...cq } = useClusters();
-    const { data: keyspaces = [], ...kq } = useKeyspaces();
 
     const [formState, setFormState] = useState<FormState>(DEFAULT_FORM_STATE);
 
@@ -66,14 +41,23 @@ export const CreateKeyspace = () => {
 
     useDocumentTitle('Create Keyspace');
 
+    const selectedCluster = clusters.find((c) => c.id === formState.clusterID);
+
     const mutation = useMutation<any, any, any>((req) => createKeyspace(req), {
         onSuccess: (data) => {
             history.push(`/keyspace/${data.result.keyspace.cluster.id}/${data.result.keyspace.keyspace.name}`);
         },
     });
 
+    const isValid = !!formState.clusterID && !!formState.name;
+    const isDisabled = mutation.isLoading || !isValid;
+
     const onSubmit: FormEventHandler = (e) => {
         e.preventDefault();
+        if (isDisabled) {
+            return;
+        }
+
         const req: pb.ICreateKeyspaceRequest = {
             cluster_id: formState.clusterID,
             options: {
@@ -82,10 +66,6 @@ export const CreateKeyspace = () => {
         };
         mutation.mutate(req);
     };
-
-    const selectedCluster = clusters.find((c) => c.id === formState.clusterID);
-
-    console.log(mutation);
 
     return (
         <div>
@@ -114,11 +94,14 @@ export const CreateKeyspace = () => {
                     </div>
                     <div className="grid grid-cols-3">
                         <Label className="col-span-2" label="Keyspace name">
-                            <TextInput onChange={(e) => updateFormState({ name: e.target.value })} />
+                            <TextInput
+                                onChange={(e) => updateFormState({ name: e.target.value })}
+                                value={formState.name}
+                            />
                         </Label>
                     </div>
                     <div className="my-12">
-                        <Button disabled={mutation.isLoading} type="submit">
+                        <Button disabled={isDisabled} type="submit">
                             Create
                         </Button>
                     </div>
