ae6dd514c5ffc7079ff16341c0415347b3f42f50
Sara Bee <855595+doeg@users.noreply.github.com>
2021-11-17 12:54:18 -0500

    button radios
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/components/routes/createKeyspace/CreateKeyspace.tsx
+++ web/vtadmin/src/components/routes/createKeyspace/CreateKeyspace.tsx
@@ -2,7 +2,9 @@ import { FormEventHandler, useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
+import cx from 'classnames';
 
+import style from './CreateKeyspace.module.scss';
 import { createKeyspace } from '../../../api/http';
 import { useDocumentTitle } from '../../../hooks/useDocumentTitle';
 import { Button } from '../../Button';
@@ -10,7 +12,7 @@ import { ContentContainer } from '../../layout/ContentContainer';
 import { NavCrumbs } from '../../layout/NavCrumbs';
 import { WorkspaceHeader } from '../../layout/WorkspaceHeader';
 import { WorkspaceTitle } from '../../layout/WorkspaceTitle';
-import { vtadmin as pb } from '../../../proto/vtadmin';
+import { vtadmin as pb, topodata, vtctldata } from '../../../proto/vtadmin';
 import { TextInput } from '../../TextInput';
 import { Label } from '../../inputs/Label';
 import { useClusters, useKeyspaces } from '../../../hooks/api';
@@ -18,11 +20,13 @@ import { Select } from '../../inputs/Select';
 
 interface FormState {
     clusterID: string;
+    keyspaceType: topodata.KeyspaceType;
     name: string;
 }
 
 const DEFAULT_FORM_STATE: FormState = {
     clusterID: 'local',
+    keyspaceType: topodata.KeyspaceType.NORMAL,
     name: '',
 };
 
@@ -80,6 +84,53 @@ export const CreateKeyspace = () => {
 
             <ContentContainer>
                 <form className="max-w-screen-sm" onSubmit={onSubmit}>
+                    <div className="mb-8">
+                        <div className="font-bold mb-4">Keyspace type</div>
+                        <div className="grid grid-cols-2 gap-4">
+                            <label
+                                className={cx(
+                                    style.radio,
+                                    formState.keyspaceType === topodata.KeyspaceType.NORMAL ? style.active : ''
+                                )}
+                            >
+                                <input
+                                    type="radio"
+                                    id="normal"
+                                    name="keyspace-type"
+                                    defaultChecked={formState.keyspaceType === topodata.KeyspaceType.NORMAL}
+                                    onChange={() => {
+                                        updateFormState({ keyspaceType: topodata.KeyspaceType.NORMAL });
+                                    }}
+                                />
+                                <span className="font-bold ml-4">Normal</span>
+                                <div className="font-size-small ml-10 mt-2 text-gray-500">
+                                    A logical database that distributes rows into different shards.
+                                </div>
+                            </label>
+
+                            <label
+                                className={cx(
+                                    style.radio,
+                                    formState.keyspaceType === topodata.KeyspaceType.SNAPSHOT ? style.active : ''
+                                )}
+                            >
+                                <input
+                                    type="radio"
+                                    id="normal"
+                                    name="keyspace-type"
+                                    defaultChecked={formState.keyspaceType === topodata.KeyspaceType.SNAPSHOT}
+                                    onChange={() => {
+                                        updateFormState({ keyspaceType: topodata.KeyspaceType.SNAPSHOT });
+                                    }}
+                                />
+                                <span className="font-bold ml-4">Snapshot</span>
+                                <div className="font-size-small ml-10 mt-2 text-gray-500">
+                                    Recover the last backup before a specific timestamp.
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+
                     <div className="grid grid-cols-3">
                         <div className="col-span-2 mb-8">
                             <Select
