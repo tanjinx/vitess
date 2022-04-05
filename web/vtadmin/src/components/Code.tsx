0f4edf91b930fdc6266a50c251803c5e0f188d8f
Sara Bee <855595+doeg@users.noreply.github.com>
2022-04-05 16:12:29 -0400

    hmmm
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

--- web/vtadmin/src/components/Code.tsx
+++ web/vtadmin/src/components/Code.tsx
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import * as React from 'react';
+import cx from 'classnames';
+import { useEffect, useRef } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import style from './Code.module.scss';
 
@@ -22,17 +24,47 @@ interface Props {
 }
 
 export const Code = ({ code }: Props) => {
-    if (typeof code !== 'string') return null;
+    const ref = useRef(null);
+    const history = useHistory();
+    const { hash } = useLocation();
+
+    const highlightRange = parseLines(hash);
+    console.log(highlightRange);
+
+    useEffect(() => {
+        if (highlightRange?.start) {
+            const el = document.getElementById(`L${highlightRange.start}`);
+            if (el) {
+                el.scrollIntoView();
+            }
+        }
+    }, [highlightRange?.start, ref.current]);
+
+    if (typeof code !== 'string') {
+        return null;
+    }
+
+    const onClickLine = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>, line: number) => {
+        console.log('clicked', line, 'shift?', e.shiftKey);
+        history.push({ hash: `#L${line}` });
+    };
 
     const codeLines = code.split('\n');
     return (
-        <table className={style.table}>
+        <table className={style.table} ref={ref}>
             <tbody>
                 {codeLines.map((line, idx) => {
+                    const isHighlighted = highlightRange && idx >= highlightRange.start && idx <= highlightRange.end;
+
                     return (
                         <tr key={idx}>
-                            <td id={`L${idx}`} className={style.lineNumber} data-line-number={idx} />
-                            <td className={style.code}>
+                            <td
+                                id={`L${idx}`}
+                                className={style.lineNumber}
+                                data-line-number={idx}
+                                onClick={(e) => onClickLine(e, idx)}
+                            />
+                            <td className={cx(style.code, isHighlighted && style.highlighted)}>
                                 <code>{line}</code>
                             </td>
                         </tr>
@@ -41,4 +73,38 @@ export const Code = ({ code }: Props) => {
             </tbody>
         </table>
     );
+};
+
+interface LineRange {
+    start: number;
+    end: number;
+}
+
+export const parseLines = (hash: string): LineRange | null => {
+    if (hash.startsWith('#L')) {
+        const re = Array.from(hash.matchAll(/\#L(\d+)(?:-L(\d+))?/g));
+        if (re.length !== 1) {
+            return null;
+        }
+
+        const matches = re[0];
+        if (matches.length !== 3) {
+            return null;
+        }
+
+        const start = matches[1];
+        const end = matches[2] || matches[1];
+
+        const nStart = parseInt(start, 10);
+        const nEnd = parseInt(end, 10);
+
+        if (isNaN(nStart) || isNaN(nEnd)) {
+            return null;
+        }
+
+        const result = { start: nStart, end: nEnd };
+        return result;
+    }
+
+    return null;
 };
