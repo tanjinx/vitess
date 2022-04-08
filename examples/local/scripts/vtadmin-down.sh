723c7d247c8d096f73dc510dc162546afb3b8620
Sara Bee <855595+doeg@users.noreply.github.com>
2022-04-08 16:59:51 -0400

    A first, gross pass
    Signed-off-by: Sara Bee <855595+doeg@users.noreply.github.com>

---
+++ examples/local/scripts/vtadmin-down.sh
@@ -0,0 +1,7 @@
+source ./env.sh
+
+echo "Stopping vtadmin-web..."
+kill -9 `cat $VTDATAROOT/tmp/vtadmin-web.pid`
+
+echo "Stopping vtadmin-api..."
+kill -9 `cat $VTDATAROOT/tmp/vtadmin-api.pid`
--- examples/local/scripts/vtadmin-up.sh
+++ examples/local/scripts/vtadmin-up.sh
@@ -3,7 +3,10 @@
 source ./env.sh
 
 log_dir="${VTDATAROOT}/tmp"
+web_dir="../../web/vtadmin"
+
 vtadmin_api_port=14200
+vtadmin_web_port=3000
 
 vtadmin \
   --addr ":${vtadmin_api_port}" \
@@ -18,23 +21,22 @@ vtadmin \
   --rbac-config="./vtadmin/rbac.yaml" \
   --cluster "id=local,name=local,discovery=staticfile,discovery-staticfile-path=./vtadmin/discovery.json,tablet-fqdn-tmpl={{ .Tablet.Hostname }}:15{{ .Tablet.Alias.Uid }}" \
   > "${log_dir}/vtadmin-api.out" 2>&1 &
+
 vtadmin_pid=$!
-
-function cleanup() {
-  kill -9 "${vtadmin_pid}"
-
-  echo
-  echo "Shutdown complete!"
-}
-
-trap cleanup INT QUIT TERM
+echo ${vtadmin_pid} > "${log_dir}/vtadmin-api.pid"
 
 echo "vtadmin-api is running on http://localhost:${vtadmin_api_port}. Logs are in ${log_dir}/vtadmin-api.out, and its PID is ${vtadmin_pid}"
 
-(
-  cd ../../web/vtadmin &&
-  npm install &&
-  REACT_APP_VTADMIN_API_ADDRESS="http://127.0.0.1:${vtadmin_api_port}" \
+npm --prefix $web_dir install
+
+REACT_APP_VTADMIN_API_ADDRESS="http://localhost:14200" \
   REACT_APP_ENABLE_EXPERIMENTAL_TABLET_DEBUG_VARS="true" \
-    npm run start
-)
+  npm run --prefix $web_dir build
+
+"${web_dir}/node_modules/.bin/serve" --no-clipboard -l $vtadmin_web_port -s "${web_dir}/build" \
+  > "${log_dir}/vtadmin-web.out" 2>&1 &
+
+vtadmin_web_pid=$!
+echo ${vtadmin_web_pid} > "${log_dir}/vtadmin-web.pid"
+
+echo "vtadmin-web is running on http://localhost:${vtadmin_web_port}. Logs are in ${log_dir}/vtadmin-web.out, and its PID is ${vtadmin_web_pid}"
